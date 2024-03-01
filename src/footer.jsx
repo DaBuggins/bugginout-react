194fddc6000a0241334c4bb3509ea6801d88cb76
DaBuggins <109602512+DaBuggins@users.noreply.github.com>
2024-03-01 12:12:33 +0000

    Create footer.jsx

---
+++ src/footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <div className="footer" id="footer-container">
+      <div className="footer" id="footer">
+        <span className="text-muted">&copy; Buggin Out 2023</span>
+      </div>
+    </div>
+  );
+}
