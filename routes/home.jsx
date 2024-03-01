d3cd3bd2f5b320346f76f695c8c675001c29ebfc
DaBuggins <109602512+DaBuggins@users.noreply.github.com>
2024-03-01 09:01:24 +0000

    Create home.jsx

---
+++ routes/home.jsx
@@ -0,0 +1,21 @@
+export default function Home() {
+  return (
+    <>
+      <div>
+        <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+          <header className="mb-auto">
+            <div>
+              <h1 className="float-md-left mb-0">Buggin Out</h1>
+            </div>
+          </header>
+          <main className="px-3">
+            <p className="lead">
+              Welcome to Buggin Out! Jump right in and explore our many bugs.
+              Feel free to share some of your own and comment on others!
+            </p>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+}
