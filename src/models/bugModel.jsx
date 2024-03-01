9b0c082f13153ccd8d2db366f89b62de30467eeb
DaBuggins <109602512+DaBuggins@users.noreply.github.com>
2024-03-01 08:14:51 +0000

    Create bugModel.jsx

---
+++ src/models/bugModel.jsx
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const bug1 = new BugSchema();
+
+const BugSchema = new Schema(
+  {
+    name: String,
+    images: [ImageSchema],
+    geometry: {
+      type: {
+        type: String,
+        enum: ["Point"],
+        required: true,
+      },
+      coordinates: {
+        type: [Number],
+        required: true,
+      },
+    },
+    sciName: String,
+    description: String,
+    location: String,
+    author: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    reviews: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  opts
+);
